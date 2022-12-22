dfe33794710230857e5f0fd012978546fdc61f97
Jonas Tiago <jonastiago95@gmail.com>
2022-12-22 09:27:19 -0300

    feat: route get/urls/:id

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -13,7 +13,24 @@ export async function createUrlShort(req, res) {
       [url, userId, shortly]
     );
 
-    res.status(201).send({shortly});
+    res.status(201).send({ shortly });
+  } catch (errr) {
+    res.status(500).send(errr.message);
+  }
+}
+
+export async function findUrlId(req, res) {
+  const urlId = req.params.id;
+
+  try {
+    const url = await connection.query(
+      `SELECT u.id,u.shortly , u.url FROM urls u  WHERE id = $1;`,
+      [parseInt(urlId)]
+    );
+
+    if (!url.rowCount) return res.sendStatus(404);
+
+    res.status(200).send(url.rows[0]);
   } catch (errr) {
     res.status(500).send(errr.message);
   }
--- src/routers/urlRouter.js
+++ src/routers/urlRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createUrlShort } from "../controllers/urlController.js";
+import { createUrlShort, findUrlId } from "../controllers/urlController.js";
 import urlValidate from "../middlewares/urlValidateMiddleware.js";
 
 const router = Router();
 
 router.post("/urls/shorten", urlValidate, createUrlShort);
+router.get("/urls/:id", findUrlId)
 
 export default router;
