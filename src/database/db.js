0210097b382afe56854529a596e0871081cc23ff
Jonas Tiago <jonastiago95@gmail.com>
2022-12-23 16:48:45 -0300

    refact: connection db and port

--- src/database/db.js
+++ src/database/db.js
@@ -1,12 +1,12 @@
 import pkg from "pg";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const { Pool } = pkg;
 
 export const connection = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "qwe123",
-  database: "shortly",
+  connectionString: process.env.DATABASE_URL,
+  ssl:true,
 });
 
--- src/serve.js
+++ src/serve.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import authRouter from "./routers/authRouter.js";
 import urlRouter from "./routers/urlRouter.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -9,6 +12,5 @@ app.use(cors());
 app.use(authRouter);
 app.use(urlRouter);
 
-
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 app.listen(port, () => console.log(`Server running in port ${port}`));
